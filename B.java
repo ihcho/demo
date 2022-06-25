aa02f7ce1c4ce7df623187aacdd8070dff7e4ed5
Ilhyung Cho <icho@svsu.edu>
2022-06-25 16:07:18 -0400

    Second commit

---
+++ B.java
@@ -0,0 +1,14 @@
+// B.java
+class A
+{
+    public A(int i) { }
+}
+
+public class B extends A
+{
+    public B(int i) {  }
+
+    public static void main(String[] args) {
+        B b = new B(4);
+    }
+}
